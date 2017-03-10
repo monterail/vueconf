48e620dfab42d5f855673e50814a683f3de2b568
Damian Dulisz <damian.dulisz@monterail.com>
2017-03-10 15:26:09 +0100

    update twitter meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,8 @@ module.exports = {
       { property: 'og:type', content: 'website' },
       { property: 'og:description', content: 'First worldwide conference by Monterail and Evan You about Vue.js - the progressive JavaScript framework. Inspiring talks and workshops led by world experts.' },
       { property: 'og:image', content: 'img/cover.png' },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:site', content: '@vueconf' },
       { property: 'twitter:image', content: 'img/cover.png' },
       { property: 'twitter:title', content: 'VueConf 2017 – June 21–23, 2017 in Wrocław, Poland | Vue.js Conference' },
       { property: 'twitter:description', content: 'First worldwide conference by Monterail and Evan You about Vue.js - the progressive JavaScript framework. Inspiring talks and workshops led by world experts.' },
