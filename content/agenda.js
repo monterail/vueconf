6c44d76b6f2c70fa823d5d03ac013eb8f2d0cd62
Damian Dulisz <damian.dulisz@monterail.com>
2017-03-24 15:29:27 +0100

    fix typo

--- content/agenda.js
+++ content/agenda.js
@@ -40,7 +40,7 @@ export default [
       }
     ],
     authorInfo: "Software Engineer at Sainsbury’s",
-    descriptionIntro: "We will take a look at how Vue.js can work as a PWA (Progressive Web Application). PWA's are boring now, right? Every one is doing a talk about them, everyone is raving about them. Well let's do something crazy and different! Lets make it realtime.",
+    descriptionIntro: "We will take a look at how Vue.js can work as a PWA (Progressive Web Application). PWA's are boring now, right? Everyone is doing a talk about them, everyone is raving about them. Well let's do something crazy and different! Lets make it realtime.",
     description: "We will take a look at the best patterns when creating an application, and how the Vue.js ecosystem can power a PWA. Extending the PWA platform we will look how Sockets and Service Workers can coexist together. Creating a PRA (Progressive Realtime Application), allowing platforms to improve the user experience without affecting the critical download chain."
   },
   {
