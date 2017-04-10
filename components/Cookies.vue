b5f0c1d11e8006e7fcbf2d8df7d4d3d47ea722fd
Damian Dulisz <damian.dulisz@monterail.com>
2017-04-10 15:03:13 +0200

    fix link to privacy policy in cookies

--- components/Cookies.vue
+++ components/Cookies.vue
@@ -4,7 +4,7 @@
     p.cookies__message
       | We use cookies to provide you with better user experience and for statistical purposes. You can block cookies by adjusting settings on your browser. If you do not block cookies, you give your consent to use them and store them in the memory of your device. For more information see our
       = " "
-      nuxt-link(to="/privatepolicty") Privacy Policy
+      nuxt-link(to="/privacypolicy") Privacy Policy
       | .
     button.button(type="button", @click="acceptCookies") Accept
     button.button.button--dark(type="button", @click="cookiesAccepted = true") Close
