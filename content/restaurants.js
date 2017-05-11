3bd1ce782fe031a8d196216b0152bebeef9e8e77
Artur Rosa <artur.rosa@monterail.com>
2017-05-11 13:34:41 +0200

    add mapLinks to hotels and restaurants

--- content/restaurants.js
+++ content/restaurants.js
@@ -4,6 +4,7 @@ export default [
     description: 'Excellent slow food made with precision, passion and concern. Rich menu and unusual design make Mama Manousch the best place to wine and dine. You leave fully satisfied and eager to come back.',
     offer: 'Vue participants are welcomed with a glass of wine!',
     link: 'http://www.mama-manousch.pl/',
+    mapLink: 'https://www.google.pl/maps/place/Mama+Manousch+-+Food+%26+Wine/@51.1085557,17.0305213,17.79z/data=!4m5!3m4!1s0x470fc2743aaa1c0f:0xe29e3b39a32a6fb7!8m2!3d51.1084419!4d17.033048?hl=pl',
     image: '/attractions/mama_manousch.jpg'
   },
   {
@@ -11,6 +12,7 @@ export default [
     description: 'Former iconic milk bar in curious Polish People’s Republic times in Poland. Presently - cafe/info desk/culture center. New-designed space with alternative food and drinks. Famous operation center of European Capital of Culture in 2016. You also may accidentally take part in some concert, vernissage or performance.',
     offer: 'As a Vue guest you get a 10% discount.',
     link: 'http://www.wroclaw2016.pl/barbara',
+    mapLink: 'https://www.google.pl/maps/place/Barbara/@51.1075801,17.030581,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc27419c5e433:0x2ae755fa287f4d58!8m2!3d51.1075768!4d17.0327697',
     image: '/attractions/barbara.jpg'
   },
   {
@@ -18,6 +20,7 @@ export default [
     description: 'Vege/Vegan and probably the slowest food in the city. 100% natural products served in interesting and fresh way. This is a place for those who want to stimulate all senses while enjoying the meal.',
     offer: '15% discount on a two-course dinner (soup + main course)',
     link: 'https://www.facebook.com/zyznabistro/',
+    mapLink: 'https://www.google.pl/maps/place/%C5%BByzna/@51.1118043,17.0343283,17z/data=!3m1!4b1!4m5!3m4!1s0x470fe9d8adb48bf5:0x61ea2d2497bf9253!8m2!3d51.111801!4d17.036517',
     image: '/attractions/zyzna.jpg'
   },
   {
@@ -25,6 +28,7 @@ export default [
     description: 'Upscale and highly recommended cocktail bar, recognized as the best place in Wroclaw. Exclusive drinks prepared by professionals in a cosmopolitan ambience.',
     offer: '10% discount for Vue participants.',
     link: 'http://www.papabar.pl/',
+    mapLink: 'https://www.google.pl/maps/place/Papa+Bar/@51.1105525,17.0257032,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc2752df68d31:0x5ba9d7dd742e1c46!8m2!3d51.1105492!4d17.0278919',
     image: '/attractions/papa_bar.jpg'
   },
   {
@@ -32,6 +36,7 @@ export default [
     description: 'Original Mediterranean cuisine restaurant named after a small island located between Italian Sardinia and French Corsica. Interior decoration feels like southern sun, sand and sea. Start your feast with Bouillabaisse and finish trying amazing Moelleux.',
     offer: 'Vue participants are welcomed with a glass of wine!',
     link: 'http://www.lamaddalena.pl/',
+    mapLink: 'https://www.google.pl/maps/place/La+Maddalena+Restaurant/@51.1082033,17.0224433,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc20b5b3a7d65:0x14dcd77bcc6132bc!8m2!3d51.1082!4d17.024632',
     image: '/attractions/la_maddalena.jpg'
   },
   {
@@ -39,6 +44,7 @@ export default [
     description: 'Fish imported from Dutch fish markets plus local Polish vegetables together give the best sushi in Wroclaw. Well-experienced cooks use unique souse-vide method. Their excellent dishes are a combination of Polish, Scandinavian and Japanese cuisine.',
     offer: '15% discount for all Vue participants.',
     link: 'http://szajnochy11.pl/',
+    mapLink: 'https://www.google.pl/maps/place/Szajnochy+11/@51.1085171,17.0265667,17z/data=!4m12!1m6!3m5!1s0x470fc274ec9720d3:0xd09f86027380af2f!2sSzajnochy+11!8m2!3d51.1085171!4d17.0287554!3m4!1s0x470fc274ec9720d3:0xd09f86027380af2f!8m2!3d51.1085171!4d17.0287554?hl=en',
     image: '/attractions/szajnochy_11.jpg'
   },
   {
@@ -46,6 +52,7 @@ export default [
     description: 'Psychedelic Haribo bears, old retro radios, bohemian air and a huge spirits menu. Szajba is a gallery club, located in the famous Pokoyhof Passage – a business, art and meeting place. In the evening it’s also a great alternative disco.',
     offer: 'All -10% (only till 10pm)',
     link: 'http://www.szajba.wroclaw.pl/',
+    mapLink: 'https://www.google.pl/maps/place/Klubogaleria+Szajba/@51.1084877,17.0242086,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc1ff6c1d7101:0x8f1c3beb042abf02!8m2!3d51.1084844!4d17.0263973',
     image: '/attractions/szajba.jpg'
   },
   {
@@ -53,6 +60,7 @@ export default [
     description: 'Cocktail bar and burger joint located among funeral homes. Apparently this unprofitable location was used to Karavan’s advantage and turned out to be a great success. Drink prosecco (poured from the tap) while being surrounded by dance macabre paintings and funeral decorations.',
     offer: '10% discount',
     link: 'https://krvn.pl/pl/kontakt/',
+    mapLink: 'https://www.google.pl/maps/place/Karavan+Bar/@51.1096499,17.0212995,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc20b0e7d47af:0x64680dcbec22b9ed!8m2!3d51.1096466!4d17.0234882',
     image: '/attractions/karavan.jpg'
   },
   {
@@ -60,6 +68,7 @@ export default [
     description: 'Restaurant and cocktail bar recommended by Forbes Navi Travel as a great place to visit while traveling on business. Pod Papugami became an iconic place due to its unique classic Hollywood cinema style. While dining and chilling, you can watch how professional bartenders prepare your drink.',
     offer: 'Vue participants get a free starter: snack and coffee - served before the main course',
     link: 'http://podpapugami.com.pl/',
+    mapLink: 'https://www.google.pl/maps/place/Pod+Papugami+-+Restauracja+%26+Cocktail+Bar/@51.1100323,17.0289693,17z/data=!3m1!4b1!4m5!3m4!1s0x470fc275a81ad04d:0xe6997124ef851a4d!8m2!3d51.110029!4d17.031158',
     image: '/attractions/pod-papugami.jpg'
   },
   {
@@ -67,6 +76,7 @@ export default [
     description: 'Regarded as the best sushi bar in Wroclaw. Why? First of all, dishes are prepared by sushi masters, educated in tough Japanese culinary schools. Moreover, new courses are being meticulously created with the help of dietitians. This place promotes quality, health and joy. Try it.',
     offer: '10% discount',
     link: 'http://sakana.pl/pl/',
+    mapLink: 'https://www.google.pl/maps/place/Sakana+Sushi.+Bar/@51.1128921,17.029564,17z/data=!3m1!4b1!4m5!3m4!1s0x470fe9df90d57331:0x918d17a4a84bca6!8m2!3d51.1128888!4d17.0317527',
     image: '/attractions/sakana_sushi.jpg'
   }
 ]
