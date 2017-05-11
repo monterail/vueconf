3edcb4fc77f55265a04c6d9cac41495e2e671382
Artur Rosa <artur.rosa@monterail.com>
2017-05-11 12:24:03 +0200

    add missing comma

--- components/Hint.vue
+++ components/Hint.vue
@@ -3,7 +3,7 @@
   h3.hint__title {{ hint.title }}
   p {{ hint.description }}
   p.hint__instruction(v-if="hint.instruction" v-html="hint.instruction")
-  a.button.button--small(v-if="hint.link" :href="hint.link" target="BLANK") WEBPAGE
+  a.button.button--small(v-if="hint.link", :href="hint.link" target="BLANK") WEBPAGE
 </template>
 
 <script>
