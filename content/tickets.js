af0d885c5f216ec3c37768c3ebfd70b4c728bb6f
Damian Dulisz <damian.dulisz@monterail.com>
2017-06-26 12:38:47 +0200

    Update tickets.js

--- content/tickets.js
+++ content/tickets.js
@@ -3,14 +3,14 @@ export default [
     name: 'Regular Ticket',
     date: 'Conference 22-23 June',
     price: '€299 + 23% VAT',
-    disabled: false,
+    disabled: true,
     url: 'https://events.zippydesk.com/vueconf-2017/register'
   },
   {
     name: 'Workshop Ticket',
     date: 'Workshop 21 June',
     price: '€299 + 23% VAT',
-    disabled: false,
+    disabled: true,
     url: 'https://events.zippydesk.com/vueconf-2017/register'
   }
 ]
